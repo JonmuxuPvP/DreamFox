5877f5777774ec38848efee86ce7b7c78ab0603c
Jonmuxu <Jonmuxupvp@gmail.com>
2024-01-13 16:06:15 +0100

    Validated parameters

--- php/api/get-user-dreams.php
+++ php/api/get-user-dreams.php
@@ -5,28 +5,35 @@
 
     $database = new Database();
 
-    $username = $_GET["username"];
+    if (isset($_GET["username"])) {
+        $username = $_GET["username"];
 
-    $statement = "SELECT id FROM user WHERE username = '$username'";
-    $preparedStatement = $database->query($statement);
-
-    $result = $preparedStatement->fetch(PDO::FETCH_ASSOC);
-
-    if ($result) {
-        $id = $result["id"];
-        $statement = "SELECT title, content, is_lucid, date FROM dream WHERE user_id = '$id'";
+        $statement = "SELECT id FROM user WHERE username = '$username'";
         $preparedStatement = $database->query($statement);
+        $result = $preparedStatement->fetch(PDO::FETCH_ASSOC);
 
+        if ($result) {
+            $id = $result["id"];
+            $statement = "SELECT title, content, is_lucid, date FROM dream WHERE user_id = '$id'";
+            $preparedStatement = $database->query($statement);
+            $dreams = parseDreams($preparedStatement); 
+
+            $response = new Response($dreams);
+            $response->send();
+        } else {
+            echo '{"error": "Invalid username"}';
+        }
+    }
+
+    function parseDreams($preparedStatement) {
         $preparedStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Dream", array("title", "content", "is_lucid", "date"));
         $dreams = new Dreams();
+
         while ($row = $preparedStatement->fetch()) {
-        $dreams->add($row);
+            $dreams->add($row);
         }
 
-        $response = new Response($dreams);
-        $response->send();
-    } else {
-        echo '{"error": "invalid user id"}';
+        return $dreams;
     }
 
 ?>
