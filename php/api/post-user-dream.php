55453baca31198cd8162e6d8e369c1a01f4242bb
Jonmuxu <Jonmuxupvp@gmail.com>
2024-01-14 18:35:38 +0100

    Created post-user-dream

---
+++ php/api/post-user-dream.php
@@ -0,0 +1,53 @@
+<?php
+	include "../database.php";
+	include "../response.php";
+
+    if (checkParameters()) {
+        $database = new Database();
+        
+        $username = $_POST["username"];
+
+        $statement = "SELECT id, password FROM user WHERE username = '$username'";
+        $preparedStatement = $database->query($statement);
+        $result = $preparedStatement->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            $id = $result["id"];
+            $password = $result["password"];
+
+            if ($password === $_POST["password"]) {
+                $title = $_POST["title"];
+                $content = $_POST["content"];
+                $is_lucid = isset($_POST["is_lucid"]);
+
+                $content = escapeCharacters($content);
+
+                $statement = "INSERT INTO dream (title, content, is_lucid, user_id) VALUES ('$title', '$content', '$is_lucid', $id)";
+                $preparedStatement = $database->query($statement);
+            } else {
+                echo '{"error": "Invalid password"}';
+            }
+        } else {
+            echo '{"error": "Invalid/missing parameters"}';
+        }
+
+
+    } else {
+        echo '{"error": "Invalid/missing parameters"}';
+    }
+
+    function checkParameters() {
+        return isset($_POST["username"]) && 
+               isset($_POST["password"]) && 
+               isset($_POST["title"]) && 
+               isset($_POST["content"]);
+    }
+
+    function escapeCharacters($content) {
+        $content = addslashes($content);
+        $content = str_replace("\\", "\\\\", $content); # yes
+        $content = str_replace(PHP_EOL, "\\\\n", $content); # I have gone
+        $content = str_replace("\\\'", "\\'", $content); # out of my mind
+        return $content;
+    }
+?>
