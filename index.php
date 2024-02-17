1fea24d21310561d9223ecc46c08ad48bb06a876
Jonmuxu <Jonmuxupvp@gmail.com>
2024-02-17 16:50:24 +0100

    Removed form

--- index.php
+++ index.php
@@ -5,36 +5,7 @@
 
 	<body>
 		<h1>DreamFox</h1>
-		<form id="dream" action="./php/api/post-user-dream.php" method="POST">
-			<label for="username">Username</label>
-			<input type="text" id="username" name="username">
-
-			<br>
-
-			<label for="password">Password</label>
-			<input type="text" id="password" name="password">
-
-			<br>
-
-			<label for="title">Dream Title</label>
-
-			<input type="text" id="title" name="title">
-			
-			<br>
-
-			<label for="content">Dream Content</label>
-			<br>
-			<textarea form="dream" name="content" id="content" cols="30" rows="10"></textarea>
-
-			<br>
-
-			<label for="lucid">Lucid</label>
-			<input type="checkbox" id="lucid" name="is_lucid">
-
-			<br>
-
-			<input type="submit" value="Submit">
-		</form>
+		<p>WIP, come back later :3</p>	
 	</body>
 </html>
 
