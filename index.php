53cf0612d21e8aaed98bb76de8f7ad0a3e146778
Jonmuxu <Jonmuxupvp@gmail.com>
2024-02-24 21:40:23 +0100

    Added a form to print dreams

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <html>
 	<head>
 
+		<style>
+			body {
+				background-color: #121212;
+				color: #FAFAFA;
+			}
+		</style>
 	</head>
 
+
 	<body>
-		<h1>DreamFox</h1>
+		<h1>DreamFox App</h1>
+
+		<h1>Submit a dream</h1>
 		<form id="dream" action="./php/api/post-user-dream.php" method="POST">
 			<label for="username">Username</label>
 			<input type="text" id="username" name="username">
@@ -35,6 +44,37 @@
 
 			<input type="submit" value="Submit">
 		</form>
+
+		<h1>Display dreams</h1>
+		<form action="">
+			<label for="username-dream">Username</label>
+			<input type="text" id="username-dream" name="password">
+			<br>
+			<button id="submit">Print</button>
+		</form>
+
+		<script>
+			const button = document.getElementById("submit");
+			const usernameDream = document.getElementById("username-dream");
+
+			button.addEventListener("click", (event) => {
+				event.preventDefault();
+				fetch(`./php/api/get-user-dreams.php?username=${usernameDream.value}`)
+				.then(response => response.json())
+				.then(json => {
+					console.clear();
+					displayDreams(json);
+				});
+			});
+
+			function displayDreams(json) {
+				const dreams = json.dreams;
+				for (const dream of dreams) {
+					const content = dream.content.replaceAll("\\n", "\n").replaceAll('\\"', '\"'); // my past mistakes have brought me here today
+					console.log(`${dream.title}\n\n${content}\n\n${dream.date}`);
+				}
+			}
+		</script>
 	</body>
 </html>
 
